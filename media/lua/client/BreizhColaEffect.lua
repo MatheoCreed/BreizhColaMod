3a8eb94cdc967fc399f58e3997974262cadce3a9
MatheoCreed <99045535+MatheoCreed@users.noreply.github.com>
2025-06-06 00:10:10 +0200

    fonction PlayerUpdate

--- media/lua/client/BreizhColaEffect.lua
+++ media/lua/client/BreizhColaEffect.lua
@@ -13,5 +13,21 @@ function BreizMod.ApplyBreizColaEffect(player)
     -- Augmentation vitesse 
     player:getModData().BreizhSpeedBoost = true 
     player:getModData().BreizhTimer = 1800 -- Ca fait environ 30 secondes
+    Events.OnPlayerUpdate.Add(BreizMod.OnPlayerUpdate)
+end
+
+function BreizMod.OnPlayerUpdate(player)
+    if not player:getModeData().BreizhSpeedBoost then 
+        return end
+    local modData = player:getModData()
+    modData.BreizhTimer = modData.BreizhTimer - 1
     
+    -- Suppression lègère de fatigue
+    player:getStats():setFatigue(player:getStats():getFatigue() - 0.005)
+
+    if modData.BreizhTimer <= 0 then
+        modData.BreizhSpeedBoost = false 
+        player:Say("Il te faut plus de Breizh Cola mon pote")
+        Events.OnPlayerUpdate.Remove(BreizMod.OnPlayerUpdate)
+    end
 end
